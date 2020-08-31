e099539e5b79e1c63e66b08207e5f7e8772bacde
Jan Piotraschke <janpiotraschke@MacBook-Pro.local>
2020-08-31 12:30:49 +0200

    create a new DB model for writting of blog posts

--- project/main.py
+++ project/main.py
@@ -1,7 +1,13 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+
+from .models import Blog_Entry
+from . import db
+from datetime import datetime
+
+
 
 main = Blueprint('main', __name__)
 
@@ -12,6 +18,29 @@ def index():
 @main.route('/about')
 def about():
     return render_template('about.html')
+
+# erstelle einen neuen Blog Eintrag --> db Eintrag
+# erstellt eine /create-Route -> d.h. 'contact.kaizn.io/create'
+@main.route('/create', methods=('GET', 'POST'))
+def create():
+    
+    if request.method == 'POST':
+
+        title = request.form['title']
+        content = request.form['content']
+
+        blog_entry = Blog_Entry(title=title, content=content, created=datetime.utcnow())
+
+        if not title:
+            flash('Title is required!')
+        else: 
+            db.session.add(blog_entry)
+            db.session.commit()
+
+            return index()
+
+            
+    return render_template('create.html')
 
 @main.route('/profile')
 @login_required
--- project/models.py
+++ project/models.py
@@ -3,8 +3,18 @@
 from flask_login import UserMixin
 from . import db
 
+# SQLAlchemy maps Python classes to database tables
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Blog_Entry(UserMixin, db.Model):
+    # overwrite the standard table name
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.Text)
+    title = db.Column(db.Text)
+    content = db.Column(db.Text)
