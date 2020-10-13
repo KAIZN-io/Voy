89c3ba8a1c90ef2f1bb053eeca7eef24a1ad7d69
Jan Piotraschke <janpiotraschke@Jans-MacBook-Pro.local>
2020-10-13 13:29:33 +0200

    added system passwd to the db_user model

--- server/model/models.py
+++ server/model/models.py
@@ -46,6 +46,7 @@ class DB_User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    system_passwd = db.Column(db.String(100))
     abbrev = db.Column(db.Text, unique=True)
     role = db.Column(db.Text)
     active = db.Column(db.Integer)
