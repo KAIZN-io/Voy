58045dd0b57a8357cd1f967de566d75b701c848b
Jan-Luca Klees <janluca@leanatic.com>
2020-10-21 09:46:52 +0200

    Basic bundling setup with parcel
    - renamed `require.js` to `main.js`
    - installed fontaweseom as a package
    - Added a `main.scss` file that imports bootstrap and font awesome
    - removed the `bundle.js`
    - Added npmr scrpt for bundling
    - removed external styles and js from the `base.html`

---
+++ server/view/static/js/main.js
@@ -0,0 +1,5 @@
+import 'jquery';
+import 'tablefilter';
+import 'popper.js';
+import 'bootstrap';
+
--- server/view/static/js/require.js
+++
@@ -1,5 +0,0 @@
-var $ = require('jquery');
-var TableFilter = require('tablefilter');
-require('popper.js');
-require('bootstrap');
-
