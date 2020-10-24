d6389e3e21655f4fb6ffe704fcc2eb908299e764
Jan-Luca Klees <janluca@leanatic.com>
2020-10-24 16:29:57 +0200

    One JS file for running the data table in `index.html`
    As the code in the two files before would not really make sense on their
    own.

--- server/view/static/js/index.js
+++ server/view/static/js/index.js
@@ -1,6 +1,32 @@
 import TableFilter from 'tablefilter';
 
 
+/**
+ * Code for handling the modal of the data table
+ */
+$('#requery_Modal').on('show.bs.modal', function (event) {
+  let button   = $(event.relatedTarget) // Button that triggered the modal
+  let query_id = button.data('id') // Extract info from data-* attributes
+  let $modal   = $(this);
+  let $title   = $modal.find('.modal-title');
+  let $content = $modal.find('.modal-body_1');
+
+  // Set query id as value
+  $modal.find('input.query_id').val(query_id);
+
+  // Update the modal Title
+  $title.text(`Requery Dialog | Query ID: ${query_id}`);
+
+  // AJAX request -> load data for the modal
+  $data = $.get(`/modal_data/${query_id}`)
+    // and push it into the modal
+    .done($data => $content.html($data))
+})
+
+
+/**
+ * Code for creating the data table
+ */
 $(document).ready(function () {
   var filtersConfig = {
     base_path : "tablefilter/dist/tablefilter/",
