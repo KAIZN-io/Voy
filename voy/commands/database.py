248ad23331dcc2961dc06677405ac99cdfeb01f9
Jan Piotraschke <janpiotraschke@imac.localdomain>
2021-03-05 11:23:53 +0100

    voy command for initializing the database

--- voy/commands/database.py
+++ voy/commands/database.py
@@ -1,5 +1,8 @@
 import click
+# import subprocess
 from flask import Blueprint
+from flask.cli import with_appcontext
+from voy.model import db
 
 database = Blueprint('database', __name__)
 
@@ -9,3 +12,12 @@ database = Blueprint('database', __name__)
 def create(name):
     """Create a user"""
     print("Create user: {}".format(name))
+
+@database.cli.command('init')
+@with_appcontext
+def init():
+    """Initialize the database"""
+    db.create_all()
+    click.echo("Initialized the database.")
+    # subprocess.run('voy shell <(echo "ls")', shell=True)
+
