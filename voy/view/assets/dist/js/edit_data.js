6bd446c926b893857561b81d508ff3ce50b5fbe9
Jan-Luca Klees <janluca@leanatic.com>
2021-04-03 23:15:25 +0200

    Deleted bundled assets that were accidentally checked in
    Bundled code does not belong into vcs, so out it goes. I think this was
    an oversight from me, when refactoring the asset structure.

--- voy/view/assets/dist/js/edit_data.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){var t;document.getElementById("submit_table").onclick=(t=document.getElementsByClassName("needs-validation"),void Array.prototype.filter.call(t,(function(t){t.addEventListener("submit",(function(e){!1===t.checkValidity()&&(e.preventDefault(),e.stopPropagation()),t.classList.add("was-validated")}),!1)})))}));
-//# sourceMappingURL=edit_data.js.map
