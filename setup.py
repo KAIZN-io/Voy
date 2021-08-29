050d7ae0e4696b3726856ab01b4e73960156f93a
Jan-Luca Klees <email@janlucaklees.de>
2021-08-29 13:52:00 +0200

    Dependencies in `setup.py`
    In this commit, I moved all direct dependencies to the `setup.py`. Doing
    so, I also removed the pinned versions, and allowed for minor version
    upgrades. Then I reinstalled everything and updated the
    `requirements.txt`.
    
    This makes it easier to manage the packages required for voy. Because
    now we know which packages are direct requirements and which are
    indirect ones. No only makes it easier to add and remove packages, but
    also to update them.

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='voy',
+    packages=find_packages(include=['voy', 'voy.commands']),
     entry_points={
         'console_scripts': [
             'voy=voy.commands:cli'
         ],
     },
+    install_requires=[
+        # Hard requirements
+        'arrow==0.17.*',
+        'Flask==1.1.*',
+        'Flask-Breadcrumbs==0.5.*',
+        'Flask-Login==0.5.*',
+        'Flask-Mail==0.9.*',
+        'Flask-Migrate==2.6.*',
+        'Flask-SQLAlchemy==2.4.*',
+        'openpyxl==3.0.*',
+        'pandas==1.2.*',
+        'pdfkit==0.6.*',
+        'psycopg2-binary==2.8.*',
+        'python-dotenv==0.15.*',
+        'python-json-logger==2.0.*',
+        'PyYAML==5.4.*',
+    ],
 )
