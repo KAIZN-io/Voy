3e9a9d63e06779039966da18814a285f2539702d
Jan-Luca Klees <email@janlucaklees.de>
2021-09-10 07:43:00 +0200

    Rewrote the user password reset command to use guard condition
    I noticed there was quite a lot of nested code in that function. So I
    transformed it into a guard condition style. I think this is easier to
    read and to reason about.

--- voy/commands/user.py
+++ voy/commands/user.py
@@ -19,21 +19,20 @@ def reset(user_abbreviation):
     # filter the requested user
     user = DB_User.query.filter_by(abbrev=user_abbreviation).first()
 
-    if user:
-        # generate a system password with the lenght of 10 and hash it
-        new_passwd = passwd_generator(size=10)
-
-        new_passwd_hash = generate_password_hash(new_passwd, method='sha256')
-
-        # commit the new system password to the database
-        DB_User.query.filter_by(abbrev=user_abbreviation).update(
-            {"password": new_passwd_hash, "is_system_passwd": True})
-        db.session.commit()
-
-        click.echo("Reset the password of the user {}".format(user_abbreviation))
-        # print the new password
-        click.echo("The new password is : {}".format(new_passwd))
-
-    else:
+    if not user:
         click.echo("A user with the abbreviation of {} does not exist".format(user_abbreviation))
+        return
 
+    # generate a system password with the lenght of 10 and hash it
+    new_passwd = passwd_generator(size=10)
+
+    new_passwd_hash = generate_password_hash(new_passwd, method='sha256')
+
+    # commit the new system password to the database
+    DB_User.query.filter_by(abbrev=user_abbreviation).update(
+        {"password": new_passwd_hash, "is_system_passwd": True})
+    db.session.commit()
+
+    click.echo("Reset the password of the user {}".format(user_abbreviation))
+    # print the new password
+    click.echo("The new password is : {}".format(new_passwd))
