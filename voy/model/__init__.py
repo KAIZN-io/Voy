bb7c795bdca22bccbaa0afec92be39fded80cca2
Jan-Luca Klees <email@janlucaklees.de>
2021-09-28 10:28:48 +0200

    Added Query <-> QueryTag association
    Needed to put everythin in one file as otherwise it was hard to get to
    work. Had issues with circular imports.

--- voy/model/__init__.py
+++ voy/model/__init__.py
@@ -6,6 +6,6 @@ migrate = Migrate()
 
 from voy.model.db_user import DB_User
 from voy.model.qc_audit import QC_Audit
-from voy.model.queries import Queries
+from voy.model.queries import Queries, QueryTags
 from voy.model.qc_requery import QC_Requery
 from voy.model.user_management import User_Management
--- voy/model/queries.py
+++ voy/model/queries.py
@@ -2,6 +2,12 @@ from voy.model import db
 from voy.model.mixins import TimeStampMixin
 
 
+query_tag_association = db.Table('query_tag_association', db.Model.metadata,
+    db.Column('query_id', db.ForeignKey('queries.id'), primary_key=True),
+    db.Column('query_tag_id', db.ForeignKey('query_tags.id'), primary_key=True),
+)
+
+
 class Queries(TimeStampMixin, db.Model):
     # ! overwrite the standard table name with 'posts', where you will commit the data to
     __tablename__ = 'queries'
@@ -24,3 +30,32 @@ class Queries(TimeStampMixin, db.Model):
     prioritized = db.Column(db.Boolean, default=False, nullable=False)
     corrected = db.Column(db.Boolean, default=False, nullable=False)
     close = db.Column(db.Boolean, default=False, nullable=False)
+
+    tags = db.relationship(
+        'QueryTags',
+        secondary=query_tag_association,
+        back_populates='queries')
+
+
+class QueryTags(TimeStampMixin, db.Model):
+    """Modelling tags to be used for adding information to queries.
+
+    Args:
+        db ([type]): [description]
+    """
+
+    __tablename__ = 'query_tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(30), unique=True)
+    description = db.Column(db.String(500))
+
+    color = db.Column(db.String(12))
+
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+
+    queries = db.relationship(
+        Queries,
+        secondary=query_tag_association,
+        back_populates='tags')
