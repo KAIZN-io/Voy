252b2fe80f1e840a2b41b58e734224318ede4c1f
Jan-Luca Klees <email@janlucaklees.de>
2021-10-29 16:08:34 +0200

    Human readable dates in views
    Added an arrow powered DateType database type that returns an arrow
    instance rather than a normal date time object from python.
    
    This then allows me to call `.humanize()` on it in the template(s)
    rendering dates.

--- voy/model/mixins/time_stamp_mixin.py
+++ voy/model/mixins/time_stamp_mixin.py
@@ -3,6 +3,13 @@ import arrow
 from voy.model import db
 
 
+class ArrowDateTime(db.TypeDecorator):
+    impl = db.DateTime
+
+    def process_result_value(self, value, dialect):
+        return arrow.get(value)
+
 class TimeStampMixin(object):
-	created_at = db.Column(db.DateTime, default=arrow.utcnow().datetime, nullable=False)
-	updated_at = db.Column(db.DateTime, default=arrow.utcnow().datetime, nullable=False, onupdate=arrow.utcnow().datetime)
+	created_at = db.Column(ArrowDateTime, default=arrow.utcnow().datetime, nullable=False)
+	updated_at = db.Column(ArrowDateTime, default=arrow.utcnow().datetime, nullable=False, onupdate=arrow.utcnow().datetime)
+
