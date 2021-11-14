bdb659b936bb1c5ef87889768b96e4721885cd63
Jan-Luca Klees <email@janlucaklees.de>
2021-11-14 11:17:33 +0100

    Added request for loading the comments
    Slowly the ticket comment modal gets to where it should be. I now added
    a request to load the markup for the rendered comments from the backend.
    
    Added two new methods for that. One that soley does the loading, one
    that triggers the loading and then updates the content.
    
    Also, I added some styling to give the modal a fixed height. This
    prevents any jumping, when the comment markup is loaded an inserted.

--- voy/view/templates/controller/ticket/comment/modal.html.js
+++ voy/view/templates/controller/ticket/comment/modal.html.js
@@ -10,6 +10,7 @@ Alpine.data('modal_ticket_comments', () => ({
   isOpen: false,
 
   ticketId: undefined,
+  renderedComments: undefined,
 
   /**
    * Makes the modal visible.
@@ -41,7 +42,31 @@ Alpine.data('modal_ticket_comments', () => ({
   reset() {
     this.isOpen = false;
 
-    this.ticketId = undefined;
+    this.ticketId         = undefined;
+    this.renderedComments = undefined;
+  },
+
+  /**
+   * Loads the rendered HTML for the comments of the current ticket.
+   *
+   * @returns {string} The rendered comment HTML.
+   */
+  loadComments() {
+    return fetch(`/tickets/${this.ticketId}/comments/modal-content`)
+      .then( response => response.text() )
+      .catch( error => console.error(error) );
+  },
+
+  /**
+   * Loads and displays the rendered comments for the current ticket.
+   *
+   * @returns {Promise} A Promise that resolves when the component status was
+   *   updates.
+   */
+  updateComments() {
+    return this.loadComments()
+      // Update the modal content
+      .then( renderedComments => this.renderedComments = renderedComments );
   },
 
   /**
@@ -50,9 +75,10 @@ Alpine.data('modal_ticket_comments', () => ({
    * @param {Event} event The event that was triggered to open the modal.
    */
   open(event) {
-    this.show();
-
     this.ticketId = event.detail.ticketId;
+
+    this.show();
+    this.updateComments();
   },
 
   /**
