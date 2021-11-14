f90508b4372b283b0b79ffccec4a1a8a536100bc
Jan-Luca Klees <email@janlucaklees.de>
2021-11-14 10:41:05 +0100

    Open ticket comment modal with parameters
    When opening the modal, I need to know which comments to load. For that
    I need the ticket id. Here, I implemented logic to pass the ticket id to
    the Alpine component.
    
    Here's whats changed:
    - The event dispatched for opening the modal now has a payload that
      includes the ticketId. This way I can read it out in my modal
      component.
    - I save the ticket id from the open event to the state of the modal,
      when it opens.
    - I added markup for a card-modal as a base for the ticket comment modal.
      I just copied this from the Bulma documentation. I think this is a
      good base to expand on.
    - I display the ticket id in the title of the dialogue, so that I can
      verify the id from the button that triggered the open event actually
      reached the state of my component.
    - I added a method to reset the component to it's initial state. I use
      this when I close the modal. I think it is good to have one point
      where all the data is reset, this way, you are less likely to forget
      to reset anything.
    - I added comments to the methods of the modal component for better code
      clarity.

--- voy/view/templates/controller/ticket/comment/modal.html.js
+++ voy/view/templates/controller/ticket/comment/modal.html.js
@@ -4,11 +4,32 @@ import Alpine from 'alpinejs';
 Alpine.data('modal_ticket_comments', () => ({
   isOpen: false,
 
-  open(event) {
-    this.isOpen = true;
+  ticketId: undefined,
+
+  /**
+   * Resets the modal to it's initial state.
+   */
+  reset() {
+    this.isOpen = false;
+
+    this.ticketId = undefined;
   },
 
-  close(event) {
-    this.isOpen = false;
+  /**
+   * Opens and initializes the modal.
+   *
+   * @param {Event} event The event that was triggered to open the modal.
+   */
+  open(event) {
+    this.isOpen = true;
+
+    this.ticketId = event.detail.ticketId;
+  },
+
+  /**
+   * Closes the modal and resets it's data to the initial state.
+   */
+  close() {
+    this.reset();
   },
 }));
