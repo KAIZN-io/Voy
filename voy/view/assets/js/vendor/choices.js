b6aea766a263db811d2e7fc9b7db7975c6b40787
Jan-Luca Klees <email@janlucaklees.de>
2022-04-20 09:14:33 +0200

    Made sure to always focus first field in form
    For enhanced usability, I made sure to always have the first field of
    any form focussed. This way the user can start typing right away and
    does not need an additional click.

--- voy/view/assets/js/vendor/choices.js
+++ voy/view/assets/js/vendor/choices.js
@@ -2,4 +2,11 @@ import Choices from "choices.js";
 
 
 document.querySelectorAll('[data-choice-js]')
-  .forEach( input => new Choices(input) );
+  .forEach( input => new Choices(input, {
+    callbackOnInit: function() {
+      if(this.passedElement.element.autofocus) {
+        this.showDropdown();
+        this.input.focus();
+      }
+    }
+  }) );
