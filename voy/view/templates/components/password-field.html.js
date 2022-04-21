bd248b25b8e4726af6f2df22b7978e1f2d28a1ef
Jan-Luca Klees <email@janlucaklees.de>
2022-04-21 09:24:38 +0200

    Improved password field ui
    - Now having a text indicating the password strength to remove confusion
      whether the progressbar is connected to the hard conditions.
    - Improved styling. The field now has a all including border around the
      progressbar and the strength message.
    - The user can no longer reacha fantastic password unless all hard
      conditions are met.

--- voy/view/templates/components/password-field.html.js
+++ voy/view/templates/components/password-field.html.js
@@ -21,36 +21,54 @@ const conditions = [
   },
 ];
 
+const scoreMessages = {
+  0: 'That\'s not a password.',
+  1: 'Criminally weak!',
+  2: 'I hope you are not using this for you Bank.',
+  3: 'We\'re getting somehwere.',
+  4: 'Fantasitc!',
+};
+
 
 Alpine.data('password_input', () => ({
 
-  score: 0,
-  conditions: [],
-
   isTouched: false,
+
+  score: 0,
+  scoreMessage: 'Waiting for input.',
+
+  conditions: [],
   areAllConditionsMet: false,
 
   isVisible: false,
 
   init() {
-    this.updateConditions('');
+    this.conditions = this.getEvaluatedConditions('');
   },
 
   evaluatePassword(value) {
     this.isTouched = true;
 
-    this.updatePasswordScore(value);
-    this.updateConditions(value);
-
+    this.conditions = this.getEvaluatedConditions(value);
     this.areAllConditionsMet = this.conditions.every( condition => condition.isValid );
+
+    this.score = this.getPasswordScore(value);
+
+    // Make sure to only show that a password is fantastic, when it meets all
+    // hard conditions.
+    if( this.score === 4 && !this.areAllConditionsMet ) {
+      this.score = 3;
+    }
+
+    this.scoreMessage = scoreMessages[this.score];
   },
 
-  updatePasswordScore(value) {
-    this.score = zxcvbn(value).score;
+  getPasswordScore(value) {
+    return zxcvbn(value).score;
   },
 
-  updateConditions(value) {
-    this.conditions = conditions.map( condition => ({
+  getEvaluatedConditions(value) {
+    return conditions.map( condition => ({
       label: condition.label,
       isValid: condition.validator(value),
     }));
