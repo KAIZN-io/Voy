28cef9a63f7dab91737268bf881217d969bb68da
Jan-Luca Klees <email@janlucaklees.de>
2022-05-06 13:59:47 +0200

    First basic implementation of tests using Pytest
    I also opted to go with an extra `requiremetns-dev.yml` to separate dev
    and prod dependencies. This way the built version of the application can
    stay small.
    
    However there is no good intgration of the prod / dev split. I just saw
    this in another project and figured I want to try this as well.

---
+++ voy/utilities/count_busy_days.py
@@ -0,0 +1,2 @@
+def count_busy_days():
+    pass
---
+++ voy/utilities/count_busy_days_test.py
@@ -0,0 +1,7 @@
+from voy.utilities.count_busy_days import count_busy_days
+
+
+class TestCountBusyDays:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
