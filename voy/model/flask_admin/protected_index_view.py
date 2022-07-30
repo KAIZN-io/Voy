e334b47bdfb4ce9fb213a4baedce3e6fd8627f7a
Jan-Luca Klees <email@janlucaklees.de>
2022-07-30 18:54:07 +0200

    Only let admins access the Voy Admin
    I realized, that we only check for authenntication, and not for the role
    of the user.
    
    So I fixd that.

--- voy/model/flask_admin/protected_index_view.py
+++ voy/model/flask_admin/protected_index_view.py
@@ -2,11 +2,14 @@ from flask import redirect, url_for, request
 from flask_admin import AdminIndexView
 from flask_login import current_user
 
+from voy.constants import ROLE_ADMIN
+
 
 class ProtectedIndexView(AdminIndexView):
 
     def is_accessible(self):
-        return current_user.is_authenticated
+        return current_user.is_authenticated and \
+            current_user.role == ROLE_ADMIN
 
     def inaccessible_callback(self, name, **kwargs):
         return redirect(url_for('authentication_controller.login'))
